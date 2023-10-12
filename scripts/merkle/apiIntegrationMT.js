04b0fff89cdd03d0f5081a689173f93c5c28efa4
alikonuk1 <alikonuk1@gmail.com>
2023-10-12 20:41:33 +0300

    Run prettier

--- scripts/merkle/apiIntegrationMT.js
+++ scripts/merkle/apiIntegrationMT.js
@@ -7,7 +7,7 @@ const ethers = require('ethers');
 const inputData = JSON.parse(fs.readFileSync(path.join(__dirname, 'treeValues', 'apiIntegrationMT.json'), 'utf8'));
 const values = inputData.values;
 
-const tree = constructMerkleTree(values, ["address", "bytes32", "string"]);
+const tree = constructMerkleTree(values, ['address', 'bytes32', 'string']);
 const merkleRoot = tree.root;
 
 // Derive hashType from the merkle tree's name
@@ -21,31 +21,31 @@ const outputPath = path.join(outputDir, 'apiIntegrationMT.json');
 const timestamp = inputData.timestamp;
 
 if (!timestamp) {
-    throw new Error('Timestamp not provided in input data');
+  throw new Error('Timestamp not provided in input data');
 }
 
 (async () => {
-    const signature = await signEIP712Message(hashType, merkleRoot, timestamp);
-    const signerAddress = ethers.Wallet.fromMnemonic(process.env.MNEMONIC).address;
+  const signature = await signEIP712Message(hashType, merkleRoot, timestamp);
+  const signerAddress = ethers.Wallet.fromMnemonic(process.env.MNEMONIC).address;
 
-    if (!fs.existsSync(outputDir)) {
-        fs.mkdirSync(outputDir);
-    }
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+  }
 
-    let output;
-    if (fs.existsSync(outputPath)) {
-        output = JSON.parse(fs.readFileSync(outputPath, 'utf8'));
-        output.signatures = output.signatures || {};
-        output.signatures[signerAddress] = signature;
-    } else {
-        output = {
-            merkleRoot: merkleRoot,
-            timestamp: timestamp,
-            signatures: {
-                [signerAddress]: signature
-            }
-        };
-    }
+  let output;
+  if (fs.existsSync(outputPath)) {
+    output = JSON.parse(fs.readFileSync(outputPath, 'utf8'));
+    output.signatures = output.signatures || {};
+    output.signatures[signerAddress] = signature;
+  } else {
+    output = {
+      merkleRoot: merkleRoot,
+      timestamp: timestamp,
+      signatures: {
+        [signerAddress]: signature,
+      },
+    };
+  }
 
-    fs.writeFileSync(outputPath, JSON.stringify(output, null, 4));
+  fs.writeFileSync(outputPath, JSON.stringify(output, null, 4));
 })();
--- scripts/merkle/merkle-utils.js
+++ scripts/merkle/merkle-utils.js
@@ -1,4 +1,4 @@
-const { StandardMerkleTree } = require("@openzeppelin/merkle-tree");
+const { StandardMerkleTree } = require('@openzeppelin/merkle-tree');
 const { ethers } = require('ethers');
 require('dotenv').config();
 
@@ -6,33 +6,33 @@ const mnemonic = process.env.MNEMONIC;
 const wallet = ethers.Wallet.fromMnemonic(mnemonic);
 
 const domain = {
-    name: "TimestampedHashRegistry",
-    version: "1.0.0",
-    chainId: 1,
-    verifyingContract: "0x0000000000000000000000000000000000000000"
+  name: 'TimestampedHashRegistry',
+  version: '1.0.0',
+  chainId: 1,
+  verifyingContract: '0x0000000000000000000000000000000000000000',
 };
 
 const types = {
-    SignedHash: [
-        { name: "hashType", type: "bytes32" },
-        { name: "hash", type: "bytes32" },
-        { name: "timestamp", type: "uint256" }
-    ]
+  SignedHash: [
+    { name: 'hashType', type: 'bytes32' },
+    { name: 'hash', type: 'bytes32' },
+    { name: 'timestamp', type: 'uint256' },
+  ],
 };
 
 async function signEIP712Message(hashType, hash, timestamp) {
-    const message = {
-        hashType: hashType,
-        hash: hash,
-        timestamp: timestamp
-    };
+  const message = {
+    hashType: hashType,
+    hash: hash,
+    timestamp: timestamp,
+  };
 
-    const signature = await wallet._signTypedData(domain, types, message);
-    return signature;
+  const signature = await wallet._signTypedData(domain, types, message);
+  return signature;
 }
 
 function constructMerkleTree(values, dataTypes) {
-    return StandardMerkleTree.of(values, dataTypes);
+  return StandardMerkleTree.of(values, dataTypes);
 }
 
 module.exports = { signEIP712Message, constructMerkleTree };
