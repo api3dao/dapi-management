bae4f2df8b7a62ba5b2d620f7e77e1e3112a5843
alikonuk1 <alikonuk1@gmail.com>
2023-10-25 16:34:46 +0300

    Address review changes

--- README.md
+++ README.md
@@ -26,27 +26,19 @@ MNEMONIC=YOUR_MNEMONIC_HERE
 
 ## Usage
 
-1. Update the `metadata.json` file located in the `data` directory with the required values for your Merkle tree. The structure of `metadata.json` should look like this:
+1. Navigate to the respective directory under `data/` corresponding to each Merkle tree. Update the current-hash.json file with the necessary values, ensuring it has the following structure:
 
 ```json
 {
-  "rootSigners": [
-    "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-    ...
-  ],
-  "merkleTrees": {
-    "priceMT": {
-      "timestamp": 1676940000,
-      "values": [...],
-      "merkleRoot": "",
-      "signatures": {}
-    },
-    ...
-  }
+  "timestamp": 1676940000,
+  "hash": "0x1234...",
+  "signatures": {
+  },
+  "merkleTreeValues": {
+    "values": [...]
+  },
 }
 ```
-
-Each Merkle tree inside the `merkleTrees` object should have a timestamp, values array, merkleRoot, and signatures object.
 
 2. Run the script by specifying the Merkle tree's name and the `chainId` as follows:
 
@@ -54,21 +46,23 @@ Each Merkle tree inside the `merkleTrees` object should have a timestamp, values
 yarn sign [MerkleTreeName] [chainId]
 ```
 
-For instance, to sign the `Price merkle tree root` Merkle tree for a network with a `chainId` of 1, you would use:
+For instance, to sign the `dAPI fallback Merkle tree` Merkle tree for a network with a `chainId` of 1, you would use:
 
 ```bash
-yarn sign "Price merkle tree root" 1
+yarn sign "dapi fallback" 1
 ```
 
-The script will construct the Merkle tree, sign it, and update the `metadata.json` file with the Merkle root and the signature.
+This command constructs the Merkle tree based on the data, signs the root, and updates `current-hash.json` in the specified directory.
 
-3. Check the `metadata.json` file in the `data` directory to see the updated Merkle root and signature.
+3. After running the script, confirm that `current-hash.json` is updated with the new Merkle root and the signature.
 
-## Understanding the `metadata.json` Fields
+## Data File Details
 
-The `metadata.json` file contains various Merkle trees, each with its unique set of values. Here's a breakdown of each Merkle tree and its associated values:
+  - **current-hash.json:** Stores the state of the Merkle tree. Updated by the script with each execution, recording the new Merkle root, its signature, and the timestamp.
 
-### 1. Price Merkle Tree
+  - **hash-signers.json:** Holds addresses that are authorized to sign the Merkle root, ensuring the authenticity of the signatures.
+
+### 1. dAPI pricing Merkle Tree
 
 - **Values**:
   - **dAPI Name (bytes32)**: The name of the dAPI, hashed.
@@ -77,34 +71,32 @@ The `metadata.json` file contains various Merkle trees, each with its unique set
   - **Duration (uint256)**: The duration for which the price is valid.
   - **Price (uint256)**: The cost in the native currency of the chain. There is no minimum USD price.
 
-### 2. dAPI Management Merkle Tree
+### 2. dAPI management Merkle Tree
 
 - **Values**:
   - **dAPI Name (bytes32)**: The name of the dAPI, hashed.
   - **Beacon Set ID (bytes32)**: Identifier for the beacon set.
   - **dAPI Sponsor Wallet Address (address)**: Wallet address of the dAPI sponsor wallet.
 
-### 3. dAPI Fallback Merkle Tree
+### 3. dAPI fallback Merkle Tree
 
 - **Values**:
   - **dAPI Name (bytes32)**: The name of the dAPI, hashed.
   - **Nodary Beacon ID (bytes32)**: Identifier for the Nodary Beacon.
   - **Nodary Sponsor Wallet Address (address)**: Wallet address of the Nodary sponsor wallet.
 
-### 4. API Integration Merkle Tree
+### 4. Signed API URL Merkle Tree
 
 - **Values**:
   - **Airnode Address (address)**: The contract address of the Airnode.
-  - **Signed API URL (bytes32)**: The signed URL of the API, hashed. Verified by referring to the DNS records of the base URL.
-
-Make sure to structure the `metadata.json` file appropriately, keeping in mind the specific values and their purposes for each Merkle tree.
+  - **Signed API URL (bytes32)**: The signed URL of the API. Verified by referring to the DNS records of the base URL.
 
 ## Merkle Tree Types
 
 In the context of this tool, each Merkle tree is associated with a unique type. This type is not arbitrarily assigned but is derived from the tree's identifiable name using a deterministic algorithm.
 
-The following code snippet shows how we derive the type of a API integration Merkle tree:
+The following code snippet shows how we derive the type of Signed API URL Merkle Tree:
 
 ```js
-keccak256(abi.encodePacked('API integration merkle tree root'));
+keccak256(abi.encodePacked('Signed API URL Merkle Tree'));
 ```
--- scripts/signMerkleTree.js
+++ scripts/signMerkleTree.js
@@ -10,115 +10,103 @@ const wallet = ethers.Wallet.fromMnemonic(mnemonic);
 const merkleType = process.argv[2];
 const chainIdArg = process.argv[3];
 
-// Validate the provided Merkle type
-if (
-  !merkleType ||
-  ![
-    'Price merkle tree root',
-    'dAPI management merkle tree root',
-    'dAPI fallback merkle tree root',
-    'API integration merkle tree root',
-  ].includes(merkleType)
-) {
-  console.error('You must provide a valid Merkle type as an argument!');
-  process.exit(1);
+const MERKLE_TREE_MAPPING = {
+    'price': 'dapi-pricing-merkle-tree-root',
+    'dapi management': 'dapi-management-merkle-tree-root',
+    'dapi fallback': 'dapi-fallback-merkle-tree-root',
+    'api integration': 'signed-api-url-merkle-tree-root',
+};
+
+if (!MERKLE_TREE_MAPPING[merkleType]) {
+    console.error('You must provide a valid Merkle type as an argument!');
+    process.exit(1);
 }
 
-// Validate and parse the provided chainId
 if (!chainIdArg || isNaN(parseInt(chainIdArg))) {
-  console.error('You must provide a valid chainId as an argument!');
-  process.exit(1);
+    console.error('You must provide a valid chainId as an argument!');
+    process.exit(1);
 }
 const chainId = parseInt(chainIdArg);
 
-// Define the domain structure for EIP-712 signing
 const domain = {
-  name: 'HashRegistry',
-  version: '1.0.0',
-  chainId: chainId,
-  verifyingContract: '0x0000000000000000000000000000000000000000',
+    name: 'HashRegistry',
+    version: '1.0.0',
+    chainId: chainId,
+    verifyingContract: '0x0000000000000000000000000000000000000000',
 };
 
-// Define the data structure of the message to be signed
 const types = {
-  SignedHash: [
-    { name: 'hashType', type: 'bytes32' },
-    { name: 'hash', type: 'bytes32' },
-    { name: 'timestamp', type: 'uint256' },
-  ],
+    SignedHash: [
+        { name: 'hashType', type: 'bytes32' },
+        { name: 'hash', type: 'bytes32' },
+        { name: 'timestamp', type: 'uint256' },
+    ],
 };
 
-// Function to sign an EIP-712 compliant message
 async function signEIP712Message(hashType, hash, timestamp) {
-  const message = {
-    hashType: hashType,
-    hash: hash,
-    timestamp: timestamp,
-  };
+    const message = {
+        hashType: hashType,
+        hash: hash,
+        timestamp: timestamp,
+    };
 
-  const signature = await wallet._signTypedData(domain, types, message);
-  return signature;
+    const signature = await wallet._signTypedData(domain, types, message);
+    return signature;
 }
 
-// Function to construct a Merkle tree from provided values and data types
 function constructMerkleTree(values, dataTypes) {
-  return StandardMerkleTree.of(values, dataTypes);
+    return StandardMerkleTree.of(values, dataTypes);
 }
 
-// Predefined data types for each Merkle tree
 const HASH_TYPE_DATA_TYPES = {
-  'Price merkle tree root': ['bytes32', 'uint256', 'bytes', 'uint256', 'uint256'],
-  'dAPI management merkle tree root': ['bytes32', 'bytes32', 'address'],
-  'dAPI fallback merkle tree root': ['bytes32', 'bytes32', 'address'],
-  'API integration merkle tree root': ['address', 'bytes32'],
+    'price': ['bytes32', 'uint256', 'bytes', 'uint256', 'uint256'],
+    'dapi management': ['bytes32', 'bytes32', 'address'],
+    'dapi fallback': ['bytes32', 'bytes32', 'address'],
+    'api integration': ['address', 'bytes32'],
 };
 
-// Function to sign a specific Merkle tree defined by its name
 async function signMerkleTree(merkleTreeName) {
-  // Define path and read metadata.json
-  const metadataPath = path.join(__dirname, '..', 'data', 'metadata.json');
-  const metadata = JSON.parse(fs.readFileSync(metadataPath, 'utf8'));
+  const folderName = MERKLE_TREE_MAPPING[merkleTreeName];
+  const currentHashPath = path.join(__dirname, '..', 'data', folderName, 'current-hash.json');
 
-  // Extract relevant data for the specified Merkle tree
-  const treeData = metadata.merkleTrees[merkleTreeName];
-  const values = treeData.values;
+  let currentHashData = { signatures: {}, merkleTreeValues: { values: [] } };
+
+  if (fs.existsSync(currentHashPath)) {
+      const currentHashRawData = fs.readFileSync(currentHashPath, 'utf8');
+      currentHashData = JSON.parse(currentHashRawData);
+  }
+
+  const values = currentHashData.merkleTreeValues ? currentHashData.merkleTreeValues.values : [];
+  const timestamp = currentHashData.timestamp ? currentHashData.timestamp : 0;
 
   const dataTypes = HASH_TYPE_DATA_TYPES[merkleTreeName];
   if (!dataTypes) {
-    throw new Error(`Data type for ${merkleTreeName} not found`);
+      throw new Error(`Data type for ${merkleTreeName} not found`);
   }
-
-  // Construct the Merkle tree and derive its root
   const tree = constructMerkleTree(values, dataTypes);
   const merkleRoot = tree.root;
 
-  // Derive hashType from the merkle tree's name
   const hashType = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(merkleTreeName));
 
-  // Get the timestamp from metadata.json
-  const timestamp = treeData.timestamp;
-
-  // Validate timestamp existence
-  if (!timestamp) {
-    throw new Error('Timestamp not provided in metadata');
-  }
-
-  // Generate the signature for the tree
   const signature = await signEIP712Message(hashType, merkleRoot, timestamp);
-  const signerAddress = ethers.Wallet.fromMnemonic(process.env.MNEMONIC).address;
+  const signerAddress = wallet.address;
 
-  // Update the metadata file with the signature and merkle root
-  treeData.merkleRoot = merkleRoot;
-  treeData.signatures = treeData.signatures || {};
-  treeData.signatures[signerAddress] = signature;
+  currentHashData = {
+    timestamp: timestamp,
+      hash: merkleRoot,
+      signatures: {
+          ...currentHashData.signatures,
+          [signerAddress]: signature
+      },
+      merkleTreeValues: { values: values },
+  };
 
-  // Write the updated metadata back to the file
-  fs.writeFileSync(metadataPath, JSON.stringify(metadata, null, 4));
+  fs.writeFileSync(currentHashPath, JSON.stringify(currentHashData, null, 4));
 }
 
-// Sign the Merkle tree
 signMerkleTree(merkleType).catch((error) => {
   console.error(`Error processing ${merkleType}:`, error);
+  process.exit(1);
 });
 
 module.exports = { signEIP712Message, constructMerkleTree };
