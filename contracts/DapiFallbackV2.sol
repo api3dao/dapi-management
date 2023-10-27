2adaff96609beabe0e317fe5d6c4be51e80b4e33
alikonuk1 <alikonuk1@gmail.com>
2023-10-27 14:47:19 +0300

    Add NatSpec documentation

--- contracts/DapiFallbackV2.sol
+++ contracts/DapiFallbackV2.sol
@@ -8,15 +8,27 @@ import "@api3/airnode-protocol-v1/contracts/api3-server-v1/interfaces/IApi3Serve
 import "./interfaces/IHashRegistry.sol";
 import "./interfaces/IDapiFallbackV2.sol";
 
+/// @title DapiFallbackV2 contract for handling dAPI fallbacks in case of primary data feed failure.
+/// @notice This contract contains the logic for executing dAPI fallbacks
+/// and ensuring data feed continuity by utilizing Merkle proofs for verification.
+/// @dev The contract inherits from the Ownable contract of the OpenZeppelin library
+/// @dev which provides basic authorization control functions.
 contract DapiFallbackV2 is Ownable, IDapiFallbackV2 {
+    /// @notice Api3ServerV1 contract address
     address public immutable api3ServerV1;
+    /// @notice HashRegistry contract address
     address public immutable hashRegistry;
 
+    /// @notice Constants defining types of the Merkle tree roots used within the contract logic.
     bytes32 private constant _DAPI_FALLBACK_HASH_TYPE =
         keccak256(abi.encodePacked("dAPI fallback Merkle tree root"));
     bytes32 private constant _PRICE_HASH_TYPE =
         keccak256(abi.encodePacked("Price Merkle tree root"));
 
+    /// @notice Initializes the contract setting the api3ServerV1 and hashRegistry addresses.
+    /// @param _api3ServerV1 The address of the Api3ServerV1 contract.
+    /// @param _hashRegistry The address of the HashRegistry contract.
+    /// @dev The constructor requires non-zero addresses for the api3ServerV1 and hashRegistry contracts.
     constructor(address _api3ServerV1, address _hashRegistry) {
         require(
             _api3ServerV1 != address(0) && _hashRegistry != address(0),
@@ -26,8 +38,12 @@ contract DapiFallbackV2 is Ownable, IDapiFallbackV2 {
         hashRegistry = _hashRegistry;
     }
 
+    /// @notice Allows the contract to receive funds.
+    /// @dev The receive function is executed on a call to the contract with empty calldata.
     receive() external payable {}
 
+    /// @notice Allows the contract owner to withdraw funds from the contract.
+    /// @param amount The amount of funds to withdraw.
     function withdraw(uint256 amount) external override onlyOwner {
         require(amount != 0, "Amount zero");
         require(
@@ -38,6 +54,25 @@ contract DapiFallbackV2 is Ownable, IDapiFallbackV2 {
         emit Withdrawn(msg.sender, amount, address(this).balance);
     }
 
+    /// @notice Executes the dAPI fallback mechanism for data feed updates by using Merkle proofs
+    /// for verification. This function updates a dAPI's data feed if the provided proofs are valid.
+    /// @dev The function requires that, the new data feed ID is different from the current one,
+    /// the Merkle proofs for dAPI fallback parameters and price updates are valid,
+    /// the sponsor wallet's balance meets the minimum requirement; otherwise, the contract funds it.
+    /// After validations, it triggers the data feed update on the Api3Server.
+    /// @param args A structured parameter of type `ExecuteDapiFallbackArgs`
+    /// containing the necessary parameters for the dAPI fallback execution, including:
+    ///   - `dapiName`: Identifier of the dAPI.
+    ///   - `dataFeedId`: New data feed ID for the dAPI.
+    ///   - `fallbackRoot`: Root of the Merkle tree for the dAPI fallback mechanism.
+    ///   - `fallbackProof`: Merkle proof for validating the dAPI fallback parameters.
+    ///   - `updateParams`: Encoded parameters for the data update.
+    ///   - `priceRoot`: Root of the Merkle tree for the pricing data.
+    ///   - `priceProof`: Merkle proof for validating the pricing data.
+    ///   - `duration`: Time period for which the price is calculated.
+    ///   - `price`: Cost of the data feed for a given duration.
+    ///   - `sponsorWallet`: Address of the sponsor wallet,
+    /// which is funded if the balance is below the required minimum.
     function executeDapiFallback(
         ExecuteDapiFallbackArgs calldata args
     ) external override {
@@ -108,6 +143,11 @@ contract DapiFallbackV2 is Ownable, IDapiFallbackV2 {
         emit ExecutedDapiFallback(args.dapiName, args.dataFeedId, msg.sender);
     }
 
+    /// @notice Validates the Merkle tree structure by verifying its root and proofs.
+    /// @param treeType The type of the tree, denoted by its specific hash type.
+    /// @param proof Proofs for the Merkle tree leaves.
+    /// @param root The known root of the Merkle tree.
+    /// @param leaf The specific leaf node of the Merkle tree being validated.
     function _validateTree(
         bytes32 treeType,
         bytes32[] calldata proof,
--- contracts/interfaces/IDapiFallbackV2.sol
+++ contracts/interfaces/IDapiFallbackV2.sol
@@ -1,26 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+/// @title IDapiFallbackV2 - Interface defining the contract for dAPI's fallback mechanisms.
+/// @notice This interface declares the events and functions required to manage and execute dAPI fallbacks.
 interface IDapiFallbackV2 {
+    /// @notice Defines the arguments required to execute a dAPI fallback.
+    /// These arguments include various parameters and Merkle proofs necessary for the fallback process.
     struct ExecuteDapiFallbackArgs {
-        bytes32 dapiName;
-        bytes32 dataFeedId;
-        bytes32 fallbackRoot;
-        bytes32[] fallbackProof;
-        bytes updateParams;
-        bytes32 priceRoot;
-        bytes32[] priceProof;
-        uint256 duration;
-        uint256 price;
-        address payable sponsorWallet;
+        bytes32 dapiName; /// Unique identifier for the dAPI.
+        bytes32 dataFeedId; /// Identifier for the data feed receiving the update.
+        bytes32 fallbackRoot; /// Root of the Merkle tree representing the dAPI's fallback structure.
+        bytes32[] fallbackProof; /// Merkle proof for validating the fallback parameters.
+        bytes updateParams; /// Encoded parameters necessary for updating the data feed.
+        bytes32 priceRoot; /// Root of the Merkle tree related to the pricing data.
+        bytes32[] priceProof; /// Merkle proof for verifying the updated pricing data.
+        uint256 duration; /// Time period for which the price is calculated.
+        uint256 price; /// Cost of the data feed for a given duration.
+        address payable sponsorWallet; /// Address of the sponsor wallet for funding.
     }
 
+    /// @notice Event emitted when funds are successfully withdrawn.
+    /// @param recipient Address receiving the funds.
+    /// @param amount Amount of funds withdrawn.
+    /// @param remainingBalance Remaining balance after the withdrawal.
     event Withdrawn(
         address indexed recipient,
         uint256 amount,
         uint256 remainingBalance
     );
 
+    /// @notice Event emitted when a sponsor wallet is successfully funded.
+    /// @param sponsorWallet Address of the sponsor wallet that was funded.
+    /// @param amount Amount of funds added.
+    /// @param remainingBalance Remaining balance after funding.
+    /// @param sender Address of the party that initiated the funding.
     event FundedSponsorWallet(
         address indexed sponsorWallet,
         uint256 amount,
@@ -28,14 +41,24 @@ interface IDapiFallbackV2 {
         address sender
     );
 
+    /// @notice Event emitted when a dAPI fallback has been executed.
+    /// @param dapiName The unique identifier for the dAPI involved.
+    /// @param dataFeedId Identifier for the data feed that was updated.
+    /// @param sender Address of the party that initiated the fallback execution.
     event ExecutedDapiFallback(
         bytes32 indexed dapiName,
         bytes32 indexed dataFeedId,
         address sender
     );
 
+    /// @notice Allows the contract owner to withdraw funds from the contract.
+    /// @param amount The amount of funds to withdraw.
+    /// @dev This function should emit the Withdrawn event after a successful withdrawal.
     function withdraw(uint256 amount) external;
 
+    /// @notice Executes the dAPI fallback mechanism.
+    /// @param args Structured data representing the fallback execution requirements.
+    /// @dev This function should emit the ExecutedDapiFallback event upon successful execution.
     function executeDapiFallback(
         ExecuteDapiFallbackArgs calldata args
     ) external;
