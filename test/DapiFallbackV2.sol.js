2e457a8cba91aeb6d5f44df68d5b200d67da9a9b
alikonuk1 <alikonuk1@gmail.com>
2023-10-30 14:34:13 +0300

    Fix linting

--- test/DapiFallbackV2.sol.js
+++ test/DapiFallbackV2.sol.js
@@ -124,7 +124,7 @@ describe('DapiFallbackV2', function () {
 
     const duration = 7776000; // 90 days in seconds
     const price = hre.ethers.utils.parseEther('3');
-    const deviationThresholdInPercentage = ethers.utils.parseUnits('1', 6); // 1e6 represents 1%
+    const deviationThresholdInPercentage = hre.ethers.utils.parseUnits('1', 6); // 1e6 represents 1%
     const heartbeatInterval = 86400; // 1 day in seconds
     const deviationReference = 0;
 
