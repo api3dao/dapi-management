4de62630ccfa637d201d2cc1707a4fd496f331e1
Markus Coetzee <mks.coetzee@gmail.com>
2023-10-30 14:15:41 +0200

    Use fixed positioning for the root layout side panel

--- frontend/components/root-layout.tsx
+++ frontend/components/root-layout.tsx
@@ -16,26 +16,24 @@ interface Props {
 export default function RootLayout(props: Props) {
   return (
     <div className={`flex text-gray-800 ${inter.className}`}>
-      <aside className="w-[200px] bg-gray-100">
-        <div className="sticky top-0 w-full">
-          <nav className="flex min-h-screen flex-col p-4 text-gray-500">
-            <Link href="/" className="mb-5">
-              <Image src="/logo.png" alt="Home" width={60} height={60} priority />
-            </Link>
-            <h3 className="mb-2 font-medium">Merkle Trees</h3>
-            <div className="flex flex-col gap-2 text-sm">
-              <NavLink href="/merkle-trees/dapi-fallback">dAPI Fallback</NavLink>
-              <NavLink href="/merkle-trees/dapi-management">dAPI Management</NavLink>
-              <NavLink href="/merkle-trees/dapi-pricing">dAPI Pricing</NavLink>
-              <NavLink href="/merkle-trees/signed-api-url">Signed API URL</NavLink>
-            </div>
-            <div className="mt-auto pt-6">
-              <Account />
-            </div>
-          </nav>
-        </div>
+      <aside className="fixed left-0 top-0 z-10 h-screen w-[200px] overflow-auto border-r border-gray-200 bg-gray-100">
+        <nav className="flex min-h-screen flex-col p-4 text-gray-500">
+          <Link href="/" className="mb-5">
+            <Image src="/logo.png" alt="Home" width={60} height={60} priority />
+          </Link>
+          <h3 className="mb-2 font-medium">Merkle Trees</h3>
+          <div className="flex flex-col gap-2 text-sm">
+            <NavLink href="/merkle-trees/dapi-fallback">dAPI Fallback</NavLink>
+            <NavLink href="/merkle-trees/dapi-management">dAPI Management</NavLink>
+            <NavLink href="/merkle-trees/dapi-pricing">dAPI Pricing</NavLink>
+            <NavLink href="/merkle-trees/signed-api-url">Signed API URL</NavLink>
+          </div>
+          <div className="mt-auto pt-6">
+            <Account />
+          </div>
+        </nav>
       </aside>
-      <main role="main" className="overflow-x-auto px-10 pt-4" style={{ maxWidth: 'calc(100vw - 200px)' }}>
+      <main role="main" className="ml-[200px] flex-grow bg-white px-5 py-4">
         {props.children}
       </main>
     </div>
