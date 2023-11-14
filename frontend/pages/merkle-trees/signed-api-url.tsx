73b3be1f076f3a0610205d0c92fe871ef325e5cc
Markus Coetzee <mks.coetzee@gmail.com>
2023-11-14 16:38:58 +0200

    Add initial Signed API URL MT page

--- frontend/pages/merkle-trees/signed-api-url.tsx
+++ frontend/pages/merkle-trees/signed-api-url.tsx
@@ -1,5 +1,117 @@
+import { z } from 'zod';
+import { AlertTriangleIcon } from 'lucide-react';
 import RootLayout from '~/components/root-layout';
+import { Table, TableHeader, TableBody, TableRow, TableHead, TableCell } from '~/components/ui/table';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
+import { Button } from '~/components/ui/button';
+import { TreeStatusBadge, TreeRootBadge, SignatureTable, TreeDiff } from '~/components/merkle-tree-elements';
+import { useWeb3Data } from '~/contexts/web3-data-context';
+import { readTreeDataFrom, readSignerDataFrom, createFileDiff } from '~/lib/server/file-utils';
+import { createSignedApiUrlMerkleTree, validateTreeRootSignatures } from '~/lib/merkle-tree-utils';
+import { InferGetServerSidePropsType } from 'next';
+import { useTreeSigner } from '~/components/merkle-tree-elements/use-tree-signer';
 
-export default function SignedApiUrlTree() {
-  return <RootLayout>TODO</RootLayout>;
+const merkleTreeSchema = z.object({
+  timestamp: z.number(),
+  signatures: z.record(z.string()),
+  merkleTreeValues: z.object({
+    values: z.array(z.tuple([z.string(), z.string()])),
+  }),
+});
+
+export async function getServerSideProps() {
+  const { path: currentTreePath, data: currentTree } = readTreeDataFrom({
+    subfolder: 'signed-api-url-merkle-tree-root',
+    file: 'current-hash.json',
+    schema: merkleTreeSchema,
+  });
+  const { path: previousTreePath, data: previousTree } = readTreeDataFrom({
+    subfolder: 'signed-api-url-merkle-tree-root',
+    file: 'previous-hash.json',
+    schema: merkleTreeSchema,
+  });
+  const { data: signers } = readSignerDataFrom('signed-api-url-merkle-tree-root');
+
+  const diffResult = previousTree ? await createFileDiff(previousTreePath, currentTreePath) : null;
+  return {
+    props: { currentTree, signers, diffResult },
+  };
+}
+
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+export default function SignedApiUrlTree(props: Props) {
+  const { currentTree, signers } = props;
+  const { address } = useWeb3Data();
+
+  const merkleTree = createSignedApiUrlMerkleTree(currentTree.merkleTreeValues.values);
+
+  const { signRoot, isSigning } = useTreeSigner('Signed API URL Merkle tree', merkleTree.root, currentTree.timestamp);
+
+  const signatures = validateTreeRootSignatures(
+    'Signed API URL Merkle tree root',
+    merkleTree.root,
+    currentTree.timestamp,
+    currentTree.signatures,
+    signers
+  );
+
+  const isSigner = !!signatures[address];
+  const canSign = signatures[address] === '0x' && !isSigning;
+
+  return (
+    <RootLayout>
+      <div>
+        <TreeStatusBadge signatures={signatures} />
+      </div>
+      <h1 className="mb-2 text-3xl font-bold">Signed API URL Merkle Tree</h1>
+      <TreeRootBadge className="mb-3" root={merkleTree.root} />
+
+      <div className="mb-10">
+        <div className="flex gap-3">
+          <Button disabled={!canSign} className="min-w-[15ch]" onClick={() => signRoot()}>
+            Sign Root
+          </Button>
+        </div>
+        {!!address && !isSigner && (
+          <p className="mt-0.5 flex items-center gap-1 text-xs text-gray-400">
+            <AlertTriangleIcon className="w-4 text-gray-300" />
+            You are not a signer.
+          </p>
+        )}
+      </div>
+
+      <div className="mb-10">
+        <SignatureTable signers={signers} signatures={signatures} />
+      </div>
+
+      <Tabs defaultValue="0">
+        <TabsList>
+          <TabsTrigger value="0">Tree Values</TabsTrigger>
+          <TabsTrigger value="1">Tree Diff</TabsTrigger>
+        </TabsList>
+        <TabsContent value="0">
+          <Table className="mt-4">
+            <TableHeader sticky>
+              <TableRow>
+                <TableHead>Airnode Address</TableHead>
+                <TableHead>Signed API URL</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {currentTree.merkleTreeValues.values.map((fallback, i) => (
+                <TableRow key={i}>
+                  <TableCell>{fallback[0]}</TableCell>
+                  <TableCell>{fallback[1]}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TabsContent>
+        <TabsContent value="1" forceMount>
+          <TreeDiff diffResult={props.diffResult} />
+        </TabsContent>
+      </Tabs>
+    </RootLayout>
+  );
 }
