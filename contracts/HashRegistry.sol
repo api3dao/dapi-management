8be8fe6b098ad7d3ad4534d3edc4ca3b3da7f784
Burak Benligiray <bbenligiray@gmail.com>
2023-11-25 14:24:22 +0300

    Validate hash type

--- contracts/HashRegistry.sol
+++ contracts/HashRegistry.sol
@@ -43,6 +43,7 @@ contract HashRegistry is Ownable, SelfMulticall, IHashRegistry {
         bytes32 hashType,
         address[] calldata signers
     ) external override onlyOwner {
+        require(hashType != bytes32(0), "Hash type zero");
         require(signers.length != 0, "Signers is empty");
         require(
             hashTypeToSigners[hashType].length() == 0,
