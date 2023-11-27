1c82f9359185ecf6f7f53c1b71379ac1bec93867
Markus Coetzee <mks.coetzee@gmail.com>
2023-11-27 17:13:57 +0200

    Make dAPI pricing MT values human-readable

--- frontend/pages/merkle-trees/dapi-pricing.tsx
+++ frontend/pages/merkle-trees/dapi-pricing.tsx
@@ -1,4 +1,6 @@
 import { z } from 'zod';
+import round from 'lodash/round';
+import { CHAINS } from '@api3/chains';
 import RootLayout from '~/components/root-layout';
 import { Table, TableHeader, TableBody, TableRow, TableHead, TableCell } from '~/components/ui/table';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
@@ -9,8 +11,8 @@ import {
   TreeDiff,
   SignRootButton,
 } from '~/components/merkle-tree-elements';
-import { readTreeDataFrom, readSignerDataFrom, createFileDiff } from '~/lib/server/file-utils';
-import { createDapiPricingMerkleTree, validateTreeRootSignatures } from '~/lib/merkle-tree-utils';
+import { readTreeDataFrom, readSignerDataFrom, createTreeDiff } from '~/lib/server/file-utils';
+import { validateTreeRootSignatures } from '~/lib/merkle-tree-utils';
 import { InferGetServerSidePropsType } from 'next';
 import { useTreeSigner } from '~/components/merkle-tree-elements/use-tree-signer';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '~/components/ui/select';
@@ -21,6 +23,7 @@ type Unit = 'wei' | 'ether';
 
 const merkleTreeSchema = z.object({
   timestamp: z.number(),
+  hash: z.string(),
   signatures: z.record(z.string()),
   merkleTreeValues: z.object({
     values: z.array(z.tuple([z.string(), z.string(), z.string(), z.string(), z.string()])),
@@ -28,19 +31,35 @@ const merkleTreeSchema = z.object({
 });
 
 export async function getServerSideProps() {
-  const { path: currentTreePath, data: currentTree } = readTreeDataFrom({
+  const { data: currentTree } = readTreeDataFrom({
     subfolder: 'dapi-pricing-merkle-tree-root',
     file: 'current-hash.json',
     schema: merkleTreeSchema,
   });
-  const { path: previousTreePath, data: previousTree } = readTreeDataFrom({
+  const { data: previousTree } = readTreeDataFrom({
     subfolder: 'dapi-pricing-merkle-tree-root',
     file: 'previous-hash.json',
     schema: merkleTreeSchema,
   });
   const { data: signers } = readSignerDataFrom('dapi-pricing-merkle-tree-root');
 
-  const diffResult = previousTree ? await createFileDiff(previousTreePath, currentTreePath) : null;
+  const diffResult = previousTree
+    ? await createTreeDiff({
+        subfolder: 'dapi-pricing-merkle-tree-root',
+        previousData: previousTree,
+        currentData: currentTree,
+        preProcessor: (values) => {
+          return [
+            ethers.utils.parseBytes32String(values[0]),
+            getChainAlias(values[1]),
+            formatUpdateParams(values[2]),
+            formatDuration(values[3]),
+            ethers.utils.commify(ethers.utils.formatEther(values[4])),
+          ] as const;
+        },
+      })
+    : null;
+
   return {
     props: { currentTree, signers, diffResult },
   };
@@ -52,13 +71,11 @@ export default function DapiPricingTree(props: Props) {
   const { currentTree, signers } = props;
   const [priceUnit, setPriceUnit] = useState<Unit>('ether');
 
-  const merkleTree = createDapiPricingMerkleTree(currentTree.merkleTreeValues.values);
-
-  const { signRoot, isSigning } = useTreeSigner('dAPI pricing Merkle tree', merkleTree.root, currentTree.timestamp);
+  const { signRoot, isSigning } = useTreeSigner('dAPI pricing Merkle tree', currentTree.hash, currentTree.timestamp);
 
   const signatures = validateTreeRootSignatures(
     'dAPI pricing Merkle tree root',
-    merkleTree.root,
+    currentTree.hash,
     currentTree.timestamp,
     currentTree.signatures,
     signers
@@ -74,7 +91,7 @@ export default function DapiPricingTree(props: Props) {
         <TreeStatusBadge signatures={signatures} />
       </div>
       <h1 className="mb-2 text-3xl font-bold">dAPI Pricing Merkle Tree</h1>
-      <TreeRootBadge className="mb-3" root={merkleTree.root} />
+      <TreeRootBadge className="mb-3" root={currentTree.hash} />
 
       <div className="mb-10">
         <SignRootButton signatures={signatures} signRoot={signRoot} isSigning={isSigning} />
@@ -96,10 +113,10 @@ export default function DapiPricingTree(props: Props) {
                 <TableHead>dAPI Name</TableHead>
                 <TableHead>Chain ID</TableHead>
                 <TableHead>dAPI Update Parameters</TableHead>
-                <TableHead>Duration</TableHead>
+                <TableHead>Duration (Days)</TableHead>
                 <TableHead>
                   Price
-                  <Select onValueChange={(unit: Unit) => setPriceUnit(unit)} defaultValue="ether">
+                  <Select value={priceUnit} onValueChange={(unit: Unit) => setPriceUnit(unit)}>
                     <SelectTrigger className="ml-1 inline-flex h-8 w-[100px]">
                       <SelectValue />
                     </SelectTrigger>
@@ -114,10 +131,10 @@ export default function DapiPricingTree(props: Props) {
             <TableBody>
               {currentTree.merkleTreeValues.values.map((pricing, i) => (
                 <TableRow key={i}>
-                  <TableCell>{pricing[0]}</TableCell>
-                  <TableCell>{pricing[1]}</TableCell>
-                  <TableCell>{pricing[2]}</TableCell>
-                  <TableCell>{pricing[3]}</TableCell>
+                  <TableCell>{ethers.utils.parseBytes32String(pricing[0])}</TableCell>
+                  <TableCell>{getChainAlias(pricing[1])}</TableCell>
+                  <TableCell>{formatUpdateParams(pricing[2])}</TableCell>
+                  <TableCell>{formatDuration(pricing[3])}</TableCell>
                   <TableCell>{convertPrice(pricing[4])}</TableCell>
                 </TableRow>
               ))}
@@ -130,4 +147,26 @@ export default function DapiPricingTree(props: Props) {
       </Tabs>
     </RootLayout>
   );
+}
+
+function formatUpdateParams(text: string) {
+  const decodedResults = ethers.utils.defaultAbiCoder.decode(['uint256', 'int224', 'uint32'], text);
+  const joinedResults = decodedResults.map((e) => e.toString()).join(', ');
+  return `(${joinedResults})`;
+}
+
+function formatDuration(seconds: string) {
+  return round(parseInt(seconds, 10) / 24 / 60 / 60, 2).toString();
+}
+
+let aliases: Record<string, string>;
+function getChainAlias(chainId: string) {
+  if (!aliases) {
+    aliases = CHAINS.reduce((acc, chain) => {
+      acc[chain.id] = chain.alias;
+      return acc;
+    }, {} as Record<string, string>);
+  }
+
+  return aliases[chainId] || chainId;
 }
--- scripts/utils.js
+++ scripts/utils.js
@@ -12,14 +12,7 @@ function createDapiManagementMerkleTree(values) {
 }
 
 function createDapiPricingMerkleTree(values) {
-  const formattedValues = values.map((value) => [
-    ethers.utils.formatBytes32String(value[0]),
-    value[1],
-    value[2],
-    value[3],
-    value[4],
-  ]);
-  return StandardMerkleTree.of(formattedValues, ['bytes32', 'uint256', 'bytes', 'uint256', 'uint256']);
+  return StandardMerkleTree.of(values, ['bytes32', 'uint256', 'bytes', 'uint256', 'uint256']);
 }
 
 function createSignedApiUrlMerkleTree(values) {
