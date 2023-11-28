f43b4bbc2fc14a1499c3fd2c301ad3f14ba3f136
Pieter du Buisson <pieter@chainapi.com>
2023-11-28 16:54:31 +0200

    include file for signatures

--- scripts/split-dapi-pricing.js
+++ scripts/split-dapi-pricing.js
@@ -8,10 +8,10 @@ async function splitDapiPricing() {
   const currentHashPath = path.join(__dirname, '..', 'data', 'dapi-pricing-merkle-tree-root', 'current-hash.json');
 
   const currentHashData = JSON.parse(fs.readFileSync(currentHashPath, 'utf8'));
-
   const values = currentHashData.merkleTreeValues.values;
+  const { merkleTreeValues, ...metdata} = currentHashData
 
-  const tree = createDapiPricingMerkleTree(currentHashData.merkleTreeValues.values);
+  const tree = createDapiPricingMerkleTree(merkleTreeValues.values);
   const valuesByChainAndDapiName = values.reduce((accumulator, item, idx) => {
     const [dapiName, chainId] = item;
     const name = dapiName.replace('/', '-');
@@ -37,14 +37,17 @@ async function splitDapiPricing() {
   for (const chainId in valuesByChainAndDapiName) {
     for (const dapiName in valuesByChainAndDapiName[chainId]) {
       const valueCollection = valuesByChainAndDapiName[chainId][dapiName];
-      let content = { merkleTreeRoot: tree.root, leaves: valueCollection };
 
       const dirPath = path.join(__dirname, '..', 'data', 'dapi-pricing-merkle-tree-root', chainId);
       const filePath = path.join(dirPath, `${dapiName}.json`);
       await fs.promises.mkdir(dirPath, { recursive: true });
-      fs.writeFileSync(filePath, JSON.stringify(content, null, 4));
+      fs.writeFileSync(filePath, JSON.stringify(valueCollection, null, 4));
     }
   }
+
+  const metadatPath = path.join(__dirname, '..', 'data', 'dapi-pricing-merkle-tree-root', 'metadata.json');
+  fs.writeFileSync(metadatPath, JSON.stringify(metdata, null, 4));
+
 
   exec('yarn format');
 }
