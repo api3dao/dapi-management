282d2ed3a3a557a8b9f522145dbedcef70111a08
metobom <unutkan2013@yandex.com>
2023-12-04 14:39:05 +0300

    Make requested changes

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,18 +1,26 @@
 const { ethers } = require('ethers');
-
 const { StandardMerkleTree } = require('@openzeppelin/merkle-tree');
 const { getAirnodeAddressByAlias, deriveDataFeedId } = require('@api3/api-integrations');
 
 function createDapiFallbackMerkleTree(values) {
-  return StandardMerkleTree.of(values, ['bytes32', 'bytes32', 'address']);
+  const formattedValues = values.map((value) => [ethers.utils.formatBytes32String(value[0]), value[1], value[2]]);
+  return StandardMerkleTree.of(formattedValues, ['bytes32', 'bytes32', 'address']);
 }
 
 function createDapiManagementMerkleTree(values) {
-  return StandardMerkleTree.of(values, ['bytes32', 'bytes32', 'address']);
+  const formattedValues = values.map((value) => [value[0], value[1], value[2]]);
+  return StandardMerkleTree.of(formattedValues, ['bytes32', 'bytes32', 'address']);
 }
 
 function createDapiPricingMerkleTree(values) {
-  return StandardMerkleTree.of(values, ['bytes32', 'uint256', 'bytes', 'uint256', 'uint256']);
+  const formattedValues = values.map((value) => [
+    ethers.utils.formatBytes32String(value[0]),
+    value[1],
+    value[2],
+    value[3],
+    value[4],
+  ]);
+  return StandardMerkleTree.of(formattedValues, ['bytes32', 'uint256', 'bytes', 'uint256', 'uint256']);
 }
 
 function createSignedApiUrlMerkleTree(values) {
@@ -28,27 +36,10 @@ function deriveBeaconSetId(dataFeedName, apiProviders) {
   return ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(['bytes32[]'], [dataFeedIds]));
 }
 
-function getDapiFallbackHashType() {
-  return ethers.utils.solidityKeccak256(['string'], ['dAPI fallback Merkle tree root']);
-}
-function getDapiManagementHashType() {
-  return ethers.utils.solidityKeccak256(['string'], ['dAPI management Merkle tree root']);
-}
-function getDapiPricingHashType() {
-  return ethers.utils.solidityKeccak256(['string'], ['dAPI pricing Merkle tree root']);
-}
-function getSignedApiUrlHashType() {
-  return ethers.utils.solidityKeccak256(['string'], ['Signed API URL Merkle tree root']);
-}
-
 module.exports = {
   deriveBeaconSetId,
   createDapiFallbackMerkleTree,
   createDapiManagementMerkleTree,
   createDapiPricingMerkleTree,
   createSignedApiUrlMerkleTree,
-  getDapiFallbackHashType,
-  getDapiManagementHashType,
-  getDapiPricingHashType,
-  getSignedApiUrlHashType,
 };
