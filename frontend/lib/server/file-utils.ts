ce2a51f5a64c50f2842dcc901ee49e91ef3d2077
Markus Coetzee <mks.coetzee@gmail.com>
2023-12-05 12:20:31 +0200

    Never use the existing processed files as a cache

--- frontend/lib/server/file-utils.ts
+++ frontend/lib/server/file-utils.ts
@@ -4,9 +4,6 @@ import { promisify } from 'util';
 import { exec } from 'child_process';
 import { z } from 'zod';
 import isObject from 'lodash/isObject';
-import isEqual from 'lodash/isEqual';
-import isBefore from 'date-fns/isBefore';
-import addDays from 'date-fns/addDays';
 
 export const execute = promisify(exec);
 
@@ -90,62 +87,25 @@ export async function createTreeDiff<T extends MerkleTreeData>(options: {
   }
 
   const treeDirPath = join(process.cwd(), '../data/.processed', subfolder);
-  const metadataPath = join(treeDirPath, 'metadata.json');
   const processedCurrentHashPath = join(treeDirPath, 'current-hash.json');
   const processedPreviousHashPath = join(treeDirPath, 'previous-hash.json');
 
-  let metadata: { processedAt: string; commit: string };
   if (!existsSync(treeDirPath)) {
     mkdirSync(treeDirPath, { recursive: true });
-    metadata = { processedAt: '', commit: '' };
-    writeFileSync(metadataPath, JSON.stringify(metadata, null, 2));
-  } else {
-    metadata = JSON.parse(readFileSync(metadataPath, 'utf8'));
   }
 
-  // We keep track of the commit when caching the processed data in order to determine if it's still fresh
-  // in the future (e.g. the code for a preprocessor might have changed). This check is in addition to others.
-  const { stdout: latestCommitHash } = await execute('git log -1 --pretty=oneline -- ./');
-
-  let hasProcessed = false;
   const processAndWriteData = (path: string, treeData: T) => {
     const processedData: T = {
       ...treeData,
       merkleTreeValues: treeData.merkleTreeValues.map(preProcessor),
     };
     writeMerkleTreeData(path, processedData);
-    hasProcessed = true;
-    console.info('Processed ' + path);
   };
 
-  const syncProcessedData = (processedDataPath: string, treeData: T) => {
-    if (
-      !existsSync(processedDataPath) ||
-      metadata.commit !== latestCommitHash ||
-      // We use the cached processed data for a maximum of 1 day
-      (metadata.processedAt && isBefore(new Date(metadata.processedAt), addDays(new Date(), -1)))
-    ) {
-      return processAndWriteData(processedDataPath, treeData);
-    }
+  processAndWriteData(processedCurrentHashPath, currentData);
+  processAndWriteData(processedPreviousHashPath, previousData);
 
-    const processedData: T = JSON.parse(readFileSync(processedDataPath, 'utf8'));
-    if (
-      processedData.hash !== treeData.hash ||
-      processedData.timestamp !== treeData.timestamp ||
-      !isEqual(processedData.signatures, treeData.signatures)
-    ) {
-      return processAndWriteData(processedDataPath, treeData);
-    }
-  };
-
-  syncProcessedData(processedCurrentHashPath, currentData);
-  syncProcessedData(processedPreviousHashPath, previousData);
-
-  if (hasProcessed) {
-    const newMetadata = { processedAt: new Date().toISOString(), commit: latestCommitHash } satisfies typeof metadata;
-    writeFileSync(metadataPath, JSON.stringify(newMetadata, null, 2));
-    await execute(`yarn prettier --write ${treeDirPath}`); // We wait so that the diff is created with formatted files
-  }
+  await execute(`yarn prettier --write ${treeDirPath}`); // We wait so that the diff is created with formatted files
 
   return await createFileDiff(processedPreviousHashPath, processedCurrentHashPath);
 }
