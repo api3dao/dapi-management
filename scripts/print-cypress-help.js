a51bc09370fb7adc557e89eed3d723016555a9b3
Markus Coetzee <mks.coetzee@gmail.com>
2023-12-14 18:27:08 +0200

    Also run yarn install when running frontend in prod mode

--- scripts/print-cypress-help.js
+++ scripts/print-cypress-help.js
@@ -4,8 +4,8 @@ console.info('\n');
 console.info(chalk.underline('Remember to have:'));
 console.info(`- Hardhat running (${command('yarn run node')})`);
 console.info(
-  `- the frontend running with required env variables (${command('yarn cypress:frontend:dev')} or ${command(
-    'yarn cypress:frontend:prod'
+  `- the frontend running with required env variables (${command('yarn cypress:frontend:prod')} or ${command(
+    'yarn cypress:frontend:dev'
   )})`
 );
 console.info('\n');
