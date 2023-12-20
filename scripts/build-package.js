29082d5c11af091f542ff49c80f9633bae156ad4
peterjurco <petrojurco@gmail.com>
2023-12-20 17:36:48 +0100

    Fix prettier

--- scripts/build-package.js
+++ scripts/build-package.js
@@ -8,8 +8,7 @@ const SUPPORTED_CHAIN_IDS_PATH = `${OUTPUT_DIR}/supported-chains.json`;
 // export the supported chain IDs from the pricing merkle tree
 const data = fs.readFileSync(PRICING_MT_PATH, 'utf8');
 const jsonData = JSON.parse(data);
-const chainIds = jsonData.merkleTreeValues.map(value => value[1]);
+const chainIds = jsonData.merkleTreeValues.map((value) => value[1]);
 const uniqueChainIds = [...new Set(chainIds)];
 
 fs.writeFileSync(SUPPORTED_CHAIN_IDS_PATH, JSON.stringify(uniqueChainIds, null, 2));
-
